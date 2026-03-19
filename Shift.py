c9622bc37352f5bb58097b9f36c364344ff61a93
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-19 16:24:31 -0400

    Create Shift.py

---
+++ Shift.py
@@ -0,0 +1,6 @@
+class Shift:
+    def __init__(self, shiftId, shiftDayOfWeek,shiftStartTime,shiftEndTime):
+        self.shiftId = shiftId
+        self.shiftDayOfWeek =shiftDayOfWeek
+        self.shiftStartTime =shiftStartTime
+        self.shiftEndTime =shiftEndTime
