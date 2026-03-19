0e61e5f8a175ed401ca51fa210f131a074672960
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-19 16:24:12 -0400

    Create Schedule.py

---
+++ Schedule.py
@@ -0,0 +1,8 @@
+class Schedule:
+    def __init__(self, scheduleId,weekStartDate, weekEndDate, scheduleStatus,departmentId,totalEmployeesPerDay):
+        self.scheduleId =scheduleId
+        self.weekStartDate= weekStartDate
+        self.weekEndDate =weekEndDate
+        self.scheduleStatus= scheduleStatus
+        self.departmentId =departmentId
+        self.totalEmployeesPerDay =totalEmployeesPerDay
