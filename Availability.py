2bdaf0222d71133f1f2ef7e2d32479d53e43e9b8
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-19 16:24:52 -0400

    Create Availability.py

---
+++ Availability.py
@@ -0,0 +1,8 @@
+class Availability:
+    def __init__(self,availabilityId,employeeId, date, startTime,endTime,status):
+        self.availabilityId= availabilityId
+        self.employeeId =employeeId
+        self.date= date
+        self.startTime= startTime
+        self.endTime= endTime
+        self.status= status
