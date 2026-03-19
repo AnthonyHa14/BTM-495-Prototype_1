b1db50cfc3b4f29d3523c332a476b9532b607ab2
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-19 16:23:30 -0400

    Create Incident Form.py

---
+++ Incident Form.py
@@ -0,0 +1,7 @@
+class IncidentForm:
+    def __init__(self, incidentId,userId, date,description, status):
+        self.incidentId =incidentId
+        self.userId =userId
+        self.date =date
+        self.description =description
+        self.status =status
