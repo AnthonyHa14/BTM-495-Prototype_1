b4c36a508da944ab53cf8c77c95977b9ee563b7f
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-19 16:25:14 -0400

    Create Task.py

---
+++ Task.py
@@ -0,0 +1,8 @@
+class Task:
+    def __init__(self, taskId, taskName,taskDescription, status,timeSpent, assignedDate):
+        self.taskId= taskId
+        self.taskName =taskName
+        self.taskDescription =taskDescription
+        self.status =status
+        self.timeSpent= timeSpent
+        self.assignedDate= assignedDate
