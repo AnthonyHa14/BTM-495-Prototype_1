a03d4dedd7ccdfa338711bb8d3b3e98a66638b02
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-19 16:26:25 -0400

    Create Timesheet.py

---
+++ Timesheet.py
@@ -0,0 +1,6 @@
+class Timesheet:
+    def __init__(self,timesheetId,shiftId, employeeId,totalHoursWorked):
+        self.timesheetId= timesheetId
+        self.shiftId= shiftId
+        self.employeeId =employeeId
+        self.totalHoursWorked =totalHoursWorked
