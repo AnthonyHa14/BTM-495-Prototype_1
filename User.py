2a0ae3ee7f6b90ce2c2c0209cbc9e75f145eb3c2
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-19 16:20:45 -0400

    Create User.py

---
+++ User.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, userId,firstName, lastName,phoneNumber,email,password,dob):
+        self.userId = userId
+        self.firstName= firstName
+        self.lastName= lastName
+        self.phoneNumber= phoneNumber
+        self.email =email
+        self.password =password
+        self.dob =dob
+
