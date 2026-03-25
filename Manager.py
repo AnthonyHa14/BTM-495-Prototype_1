9b366154d49719bc02859f6b8ef16e945632fc22
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-25 17:42:25 -0400

    Update Manager.py

--- Manager.py
+++ Manager.py
@@ -3,3 +3,39 @@ class Manager(User):
         super().__init__(userId, firstName,lastName, phoneNumber,email, password,dob)
         self.permission= permission
         self.yearlySalary =yearlySalary
+        
+#Create schedule draft
+    def createScheduleDraft(self, shiftId, scheduleId, userId):
+        print("Schedule draft created.")
+        return True
+        
+ # Publish schedule
+    def publishSchedule(self, scheduleId, userId):
+        print("Schedule published.")
+        return True
+
+#Create message text
+    def createMessage(self, message):
+        print("Message created.")
+        return message
+
+# Select employees
+    def selectEmployees(self, userId):
+        print(f"Employee {userId} selected.")
+        return True
+        
+#Send notification
+    def sendNotification(self, message):
+        notification = Notification( 1, message,self.userId,"sent","2026-03-25")
+        print("Notification sent.")
+        return notification
+
+# Approve shift change
+    def approveShiftChanges(self, shiftId, userId):
+        print("Shift change approved.")
+        return True
+
+ # Approve timesheet
+    def approveTimesheet(self, timesheetId):
+        print("Timesheet approved.")
+        return True
