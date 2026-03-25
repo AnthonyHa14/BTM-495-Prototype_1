86e91ac68b9ee1935baf1a44c5a588357b6fbb21
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-25 17:48:04 -0400

    Update Address.py

--- Address.py
+++ Address.py
@@ -3,3 +3,26 @@ class Address:
         self.postalCode= postalCode
         self.streetName = streetName
         self.province =province
+
+# Update address
+    def updateAddress(self,postalCode,streetName,province):
+        self.postalCode = postalCode
+        self.streetName = streetName
+        self.province = province
+        print("Address updated.")
+        return True
+
+ # Get full address
+    def getFullAddress(self):
+        fullAddress = ( self.streetName + ", " +self.province +", " +self.postalCode )
+        print(fullAddress)
+        return fullAddress
+
+ # Validate postal code
+    def validatePostalCode(self):
+        if self.postalCode != "":
+            print("Postal code valid.")
+            return True
+        else:
+            print("Postal code invalid.")
+            return False
