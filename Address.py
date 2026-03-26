3769fefb977d33ca8dedc4fcaf098b919ba37c8b
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-26 11:52:22 -0400

    Update Address.py
    Added type hints for postalCode, streetName, and province in the Address class.

--- Address.py
+++ Address.py
@@ -1,9 +1,10 @@
 class Address:
-    def __init__(self, postalCode,streetName, province):
-        self.postalCode= postalCode
-        self.streetName = streetName
-        self.province =province
+    def __init__(self,postalCode: str,streetName: str, province: str):
+        self.postalCode:str =postalCode
+        self.streetName: str=streetName
+        self.province: st =province
 
+    
 # Update address
     def updateAddress(self,postalCode,streetName,province):
         self.postalCode = postalCode
