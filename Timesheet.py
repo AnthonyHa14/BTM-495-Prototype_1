0e864b163f877a9ba09cdc8dc3f8ad1921a05b8a
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-26 11:07:46 -0400

    Update Timesheet.py

--- Timesheet.py
+++ Timesheet.py
@@ -4,3 +4,29 @@ class Timesheet:
         self.shiftId= shiftId
         self.employeeId =employeeId
         self.totalHoursWorked =totalHoursWorked
+
+#Calculate total hours worked
+    def calculateTotalHours(self):
+        print("Total hours worked:", self.totalHoursWorked)
+        return self.totalHoursWorked
+
+#Update total worked hours
+    def updateHours(self, totalHoursWorked):
+        self.totalHoursWorked = totalHoursWorked
+        print("Hours successfully updated")
+        return True
+
+# Submit the timesheet to payroll
+    def submitToPayroll(self):
+        if self.totalHoursWorked > 0:
+            print("Timesheet successfully submitted to payroll ")
+            return True
+        else:
+            print("Timesheet submission failed")
+            return False
+
+# Verify timesheet details
+    def verifyTimesheetDetails(self):
+        details = f"Timesheet {self.timesheetId}, Hours: {self.totalHoursWorked}"
+        print(details)
+        return details
