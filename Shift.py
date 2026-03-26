329b38ad906f3018fc59b9d052ad6a98d1d0f4fa
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-26 11:59:51 -0400

    Update Shift.py

--- Shift.py
+++ Shift.py
@@ -1,10 +1,11 @@
 class Shift:
-    def __init__(self, shiftId, shiftDayOfWeek,shiftStartTime,shiftEndTime):
-        self.shiftId = shiftId
-        self.shiftDayOfWeek =shiftDayOfWeek
-        self.shiftStartTime =shiftStartTime
-        self.shiftEndTime =shiftEndTime
-
+    def __init__(self, scheduleId: int,weekStartDate: str,weekEndDate:str,scheduleStatus:str,departmentId:int,totalEmployeesPerDay: int):
+        self.scheduleId:int =scheduleId
+        self.weekStartDate: str= weekStartDate
+        self.weekEndDate: str =weekEndDate
+        self.scheduleStatus:str = scheduleStatus
+        self.departmentId:int =departmentId
+        self.totalEmployeesPerDay:int =totalEmployeesPerDay
 
 # Create a new shift
     def createShift(self):
